a21ee1a0dc567c8e15a9ebec5a2f5952659a34ba
Wendy-Ying <143325815+Wendy-Ying@users.noreply.github.com>
2024-11-13 19:20:36 +0800

    Add files via upload

--- lab7/motor_pwm.py
+++ lab7/motor_pwm.py
@@ -8,16 +8,24 @@ GPIO.setwarnings(False)
 def set_angle(angle):
     duty = angle / 9 + 2.5
     pwm.ChangeDutyCycle(duty)
+    print(duty)
+
     
 try:
     pwm = GPIO.PWM(11,50)
     pwm.start(2.5)
+    last_angle = 0
     while True:
-        for i in range(0, 180):
-            set_angle(i)
-            time.sleep(0.01)
-    pwm.stop()
-    GPIO.cleanup()
+        angle = int(input())
+        if angle < last_angle:
+            for i in range(last_angle, angle, -1):
+                set_angle(i)
+                time.sleep(0.01)
+        elif angle > last_angle:
+            for i in range(last_angle, angle):
+                set_angle(i)
+                time.sleep(0.01)
+        last_angle = angle
 except KeyboardInterrupt:
     pwm.stop()
     GPIO.cleanup()
